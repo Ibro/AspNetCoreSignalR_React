229d364db213f4687e901adc0b7df083bbf3744f
Ibro <ibro@ibro.me>
2017-11-16 22:14:51 +0100

    Listen for sendToAll event from server

--- AspNetCoreSignalR_React.Client/src/Chat.js
+++ AspNetCoreSignalR_React.Client/src/Chat.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { HubConnection } from '@aspnet/signalr-client';
 
 class Chat extends Component {
   constructor(props) {
@@ -12,7 +13,7 @@ class Chat extends Component {
     };
   }
 
-  componentDidMount() {
+  componentDidMount = () => {
     const nick = window.prompt('Your name:', 'John');
 
     const hubConnection = new HubConnection('http://localhost:5000/chat');
@@ -22,9 +23,15 @@ class Chat extends Component {
         .start()
         .then(() => console.log('Connection started!'))
         .catch(err => console.log('Error while establishing connection :('));
+
+      this.state.hubConnection.on('sendToAll', (nick, receivedMessage) => {
+        const text = `${nick}: ${receivedMessage}`;
+        const messages = this.state.messages.concat([text]);
+        this.setState({ messages });
+      });
     });
   }
-
+  
   render() {
     return <div>Here goes chat</div>;
   }
