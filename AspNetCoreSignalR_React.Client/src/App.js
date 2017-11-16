870645250e1fd88e0b28d02732726856bfdb336f
Ibro <ibro@ibro.me>
2017-11-16 22:11:15 +0100

    Load Chat component from App component

--- AspNetCoreSignalR_React.Client/src/App.js
+++ AspNetCoreSignalR_React.Client/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import Chat from './Chat';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,9 +13,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <div>
+         <Chat />
+       </div>
       </div>
     );
   }
