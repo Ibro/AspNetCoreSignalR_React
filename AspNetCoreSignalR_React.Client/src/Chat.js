651ae634c9c36ffb0ecb828d7665d07ce187f7a4
Ibro <ibro@ibro.me>
2017-11-16 22:21:45 +0100

    Add view for Chat component

--- AspNetCoreSignalR_React.Client/src/Chat.js
+++ AspNetCoreSignalR_React.Client/src/Chat.js
@@ -39,7 +39,24 @@ class Chat extends Component {
   };
 
   render() {
-    return <div>Here goes chat</div>;
+    return (
+      <div>
+        <br />
+        <input
+          type="text"
+          value={this.state.message}
+          onChange={e => this.setState({ message: e.target.value })}
+        />
+
+        <button onClick={this.sendMessage}>Send</button>
+
+        <div>
+          {this.state.messages.map((message, index) => (
+            <span style={{display: 'block'}} key={index}> {message} </span>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
